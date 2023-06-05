3a8748fecea1c623acd3c6f963114f34f744e31d
William Wong <william.wai.lim.wong@gmail.com>
2023-06-05 11:01:33 -0400

    working lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
+  useEffect(()=> {
+      const timer = setTimeout(() => setTimeRemaining((timeRemaining) => timeRemaining -1 ), 1000);
+      if (timeRemaining==0){
+        setTimeRemaining(10);
+        onAnswered(false);
+      }
+      return function cleanup() {
+        clearTimeout(timer);
+      }
+    }
+  );
   // add useEffect code
 
   function handleAnswer(isCorrect) {
